ad207f090ffe5d0fbfc5d684262fab03b2b56356
Devin <liudev777@gmail.com>
2024-11-20 23:31:24 -0600

    insight

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Home from "./pages/Home";
 import Nav from "./components/Nav";
 // import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   const ROUTE_ENDPOINT = "/"
   return (
     <Router>
+      <SpeedInsights/>
       {/* Background */}
       <div className="glow"/>
       <div className="bg"/>
